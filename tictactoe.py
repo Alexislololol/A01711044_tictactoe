a3b1e9afe20231daebbeab14a8aff46c98ae212c
Alexis <alexisrico10188777@gmail.com>
2026-05-09 00:38:20 -0600

    Código con comentarios de los cambios realizados

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,11 @@
-"""Tic Tac Toe
+"""Tres en raya
 
-Exercises
+Ejercicios
 
-1. Give the X and O a different color and width.
-2. What happens when someone taps a taken spot?
-3. How would you detect when someone has won?
-4. How could you create a computer player?
+1. Dale a la X y a la O un color y un grosor diferentes.
+2. ¿Qué pasa cuando alguien toca una casilla ya ocupada?
+3. ¿Cómo detectarías cuando alguien ha ganado?
+4. ¿Cómo podrías crear un jugador controlado por la computadora?
 """
 
 from turtle import *
@@ -14,7 +14,7 @@ from freegames import line
 
 
 def grid():
-    """Draw tic-tac-toe grid."""
+    """Dibuja la cuadrícula del tres en raya."""
     line(-67, 200, -67, -200)
     line(67, 200, 67, -200)
     line(-200, -67, 200, -67)
@@ -22,15 +22,15 @@ def grid():
 
 
 def drawx(x, y):
-    """Draw X player."""
-    color("blue")
+    """Dibuja la X."""
+    color("blue") #usa la función color() para definir el color de las X a azul
     line(x, y, x + 133, y + 133)
     line(x, y + 133, x + 133, y)
 
 
 def drawo(x, y):
-    """Draw O player."""
-    color("red")
+    """Dibuja la O."""
+    color("red") #usa la función color() para definir el color de las O a rojo
     up()
     goto(x + 67, y + 5)
     down()
@@ -38,18 +38,20 @@ def drawo(x, y):
 
 
 def floor(value):
-    """Round value down to grid with square size 133."""
+    """Redondea el valor hacia abajo a la cuadrícula con un tamaño de cuadrado de 133."""
     return ((value + 200) // 133) * 133 - 200
 
 
 state = {'player': 0}
 players = [drawx, drawo]
 
+# state['ocupado'] guarda las casillas ya visitadas (coordenadas redondeadas) para dibujar dos piezas en la misma posición y no alternamos el turno cuando se hace click ahí
 state['ocupado'] = set()
 
 
+
 def tap(x, y):
-    """Draw X or O in tapped square.
+    """Dibuja la X o la O en la casilla tocada.
     """
     x = floor(x)
     y = floor(y)
