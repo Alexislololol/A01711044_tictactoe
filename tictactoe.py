466cb178eb5f9a90aac359cd9a99f16556a229c1
Alexis <alexisrico10188777@gmail.com>
2026-05-09 00:54:23 -0600

    Validación total con Flake8

--- tictactoe.py
+++ tictactoe.py
@@ -8,7 +8,7 @@ Ejercicios
 4. ¿Cómo podrías crear un jugador controlado por la computadora?
 """
 
-from turtle import *
+import turtle
 
 from freegames import line
 
@@ -23,57 +23,54 @@ def grid():
 
 def drawx(x, y):
     """Dibuja la X."""
-    color("blue") #usa la función color() para definir el color de las X a azul
+    turtle.color("blue")  # usa color() para definir el color de la X
     line(x, y, x + 133, y + 133)
     line(x, y + 133, x + 133, y)
 
 
 def drawo(x, y):
     """Dibuja la O."""
-    color("red") #usa la función color() para definir el color de las O a rojo
-    up()
-    goto(x + 67, y + 5)
-    down()
-    circle(62)
+    turtle.color("red")  # usa color() para definir el color de la O
+    turtle.up()
+    turtle.goto(x + 67, y + 5)
+    turtle.down()
+    turtle.circle(62)
 
 
 def floor(value):
-    """Redondea el valor hacia abajo a la cuadrícula con un tamaño de cuadrado de 133."""
+    """Redondea hacia abajo a la cuadrícula (paso 133)."""
     return ((value + 200) // 133) * 133 - 200
 
 
-state = {'player': 0}
+state = {"player": 0}
 players = [drawx, drawo]
 
-# state['ocupado'] guarda las casillas ya visitadas (coordenadas redondeadas) para dibujar dos piezas en la misma posición y no alternamos el turno cuando se hace click ahí
-state['ocupado'] = set()
-
+# state['ocupado']: casillas redondeadas ya visitadas.
+state["ocupado"] = set()
 
 
 def tap(x, y):
-    """Dibuja la X o la O en la casilla tocada.
-    """
+    """Dibuja la X o la O en la casilla tocada."""
     x = floor(x)
     y = floor(y)
 
-    if (x, y) in state['ocupado']:
+    if (x, y) in state["ocupado"]:
         return
 
-    player = state['player']
-    draw = players[player]
-    draw(x, y)
+    player = state["player"]
+    players[player](x, y)
 
-    state['ocupado'].add((x, y))
+    state["ocupado"].add((x, y))
 
-    update()
-    state['player'] = not player
-
+    turtle.update()
+    state["player"] = not player
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
+
 grid()
-update()
-onscreenclick(tap)
-done()
+turtle.update()
+turtle.onscreenclick(tap)
+turtle.done()
